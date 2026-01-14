afaddf2652432dfe158ff961dee31e25725ed69e
Teta <tetref@users.noreply.github.com>
2026-01-14 20:32:24 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/contractcore/main.go
@@ -0,0 +1,23 @@
+// cmd/contractcore/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "contractcore/internal/contractcore"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := contractcore.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
